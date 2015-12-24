a8d2dc55d1a45901e27a1431e96283b1bfe65806
yuens <ysh329@sina.com>
2015-12-24 19:33:49 +0800

    Make some new test, such as return the http code.

--- spider/test_soup.py
+++ spider/test_soup.py
@@ -1,7 +1,24 @@
 import urllib2
 from bs4 import BeautifulSoup
+from urllib2 import URLError
 
-html_doc = urllib2.urlopen("http://www.yuenshome.com").read()
+try:
+    response = urllib2.urlopen("http://www.yuenshome.com")
+    code = response.getcode()
+    print code
+    print type(code)
+    html_doc = response.read()
+except URLError, e:
+    if hasattr(e, 'code'):
+        print 'The server couldn\'t fulfill the request.'
+        print 'Error code: ', e.code
+    elif hasattr(e, 'reason'):
+        print 'We failed to reach a server.'
+        print 'Reason: ', e.reason
+else:
+    print 'No exception was raised.'
+    print urllib2.URLError
+
 soup = BeautifulSoup(html_doc, "lxml")
 print map(lambda a: a['href'], soup.find_all('a'))
 '''
